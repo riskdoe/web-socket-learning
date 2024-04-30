e9ffaaa030d17f997af00dfb6ebe836ebf62c0d4
risky <tarryntw@gmail.com>
2024-04-30 19:35:37 +1200

    test 1

---
+++ main.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI, Request, WebSocket
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+
+app = FastAPI()
+templates = Jinja2Templates(directory="templates")
+
+
+#return html file
+@app.get("/", response_class=HTMLResponse)
+async def home(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    while True:
+        await websocket.send_text(f"E")
